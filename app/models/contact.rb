c409dfdf7f03d2d5431219f819fa9b2be270e874
bikmazefe <bikmazefe@gmail.com>
2021-10-29 03:49:23 +0300

    add missing validations to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,10 +5,14 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :email, :phone, :first_name, :last_name
+  validates_length_of :first_name, maximum: 50
+  validates_length_of :last_name, maximum: 50
+  validates_length_of :phone, maximum: 20
+  validates_length_of :email, maximum: 320
   validates :email, uniqueness: {
                       scope: :user_id,
                       message: "already exists in another contact.",
-                    }
+                    }, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def self.grouped
     all.order(first_name: :asc).group_by { |c| c.first_name[0] }
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,12 +2,17 @@ require "rails_helper"
 require "models/concerns/event_loggable"
 RSpec.describe Contact, type: :model do
   it { should belong_to(:user) }
-  it_behaves_like('event_loggable')
+  it_behaves_like("event_loggable")
 
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:phone) }
+
+  it { should validate_length_of(:first_name).is_at_most(50) }
+  it { should validate_length_of(:last_name).is_at_most(50) }
+  it { should validate_length_of(:phone).is_at_most(20) }
+  it { should validate_length_of(:email).is_at_most(320) }
 
   it "validates if email is unique among user's contacts" do
     user = FactoryBot.create(:user)
